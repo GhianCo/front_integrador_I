169df0349463b5c0222de41ea990a4049088762d
GhianCo <ghiancorx@gmail.com>
2025-05-08 12:27:52 -0500

    estructura inicia;

---
+++ src/app/app.resolvers.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { NavigationService } from 'app/core/navigation/navigation.service';
+import { MessagesService } from 'app/layout/common/messages/messages.service';
+import { forkJoin } from 'rxjs';
+
+export const initialDataResolver = () => {
+    const messagesService = inject(MessagesService);
+    const navigationService = inject(NavigationService);
+
+    // Fork join multiple API endpoint calls to wait all of them to finish
+    return forkJoin([
+        navigationService.get(),
+        messagesService.getAll(),
+    ]);
+};
